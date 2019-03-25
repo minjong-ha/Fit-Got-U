18c01f4489472a65fc8d1df7304d0768b7c5aa3c
Runaind <ruinahri@gmail.com>
2019-03-25 19:03:36 +0900

    join_edited

--- MyApplication/app/src/main/java/com/example/myapplication/JoinActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/JoinActivity.java
@@ -1,7 +1,13 @@
 package com.example.myapplication;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.RadioGroup;
+import android.widget.TextView;
 
 public class JoinActivity extends AppCompatActivity {
 
@@ -9,5 +15,26 @@ public class JoinActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
+
+        final EditText join_id = (EditText)findViewById(R.id.join_id);
+        final EditText join_pw = (EditText)findViewById(R.id.join_pw);
+        final RadioGroup join_who_g = (RadioGroup)findViewById(R.id.join_who_g);
+
+        Button join_b = (Button)findViewById(R.id.join_b);
+
+        join_b.setOnClickListener(new Button.OnClickListener() {//클릭 이벤트
+            @Override
+            public void onClick(View view) {
+                Intent join_intent = new Intent(getApplicationContext(),StartPageActivity.class);
+                startActivity(join_intent);
+            }
+        });
+    }
+
+    @Override
+    public void onPause() {
+        super.onPause();
+
+        finish();//화면 사라지면 바로 종료
     }
 }
