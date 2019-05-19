607b4c8f16d6a314fe0b68b5594b910c06a84bd5
hwangshkr <ruinahri@gmail.com>
2019-05-19 22:15:11 +0900

    data analysis merge

---
+++ MyApplication/app/src/main/java/com/example/myapplication/Activity/IntroActivity.java
@@ -0,0 +1,42 @@
+package com.example.myapplication.Activity;
+
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.os.Handler;
+
+import com.example.myapplication.R;
+
+public class IntroActivity extends Activity {
+    private Handler handler;
+
+    Runnable runnable = new Runnable() {
+        @Override
+        public void run() {
+            Intent intent = new Intent(IntroActivity.this, MainActivity.class);
+            startActivity(intent);
+            finish();
+            overridePendingTransition(android.R.anim.fade_in,  android.R.anim.fade_out);
+        }
+    };
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_intro);
+        init();
+
+        handler.postDelayed(runnable, 2000);
+    }
+
+    public void init() {
+        handler = new Handler();
+    }
+
+    @Override
+    public void onBackPressed(){
+        super.onBackPressed();
+        handler.removeCallbacks(runnable);
+    }
+}
---
+++ MyApplication/app/src/main/java/com/example/myapplication/List/DA2_List_Item.java
@@ -0,0 +1,9 @@
+package com.example.myapplication.List;
+
+// pop시 나오는 일별 상세 내용
+public class DA2_List_Item {
+    public String strExercise;
+    public String ExerciseCount;
+    public String strText;
+
+}
