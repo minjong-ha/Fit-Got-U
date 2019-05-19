4eca12067bc5a06bbabbbab19cb5fcf3a7e66609
JHLee0211 <qpalzmfghtgv@naver.com>
2019-05-19 23:09:26 +0900

    운동 선택화면 디자인 변경 및 데이터분석 부분 디자인 약간 변경! 조인 라디오버튼 수정 중 (곰돌이 못잃어..)

--- MyApplication/app/src/main/java/com/example/myapplication/Activity/MainActivity.java
+++ MyApplication/app/src/main/java/com/example/myapplication/Activity/MainActivity.java
@@ -79,7 +79,7 @@ public class MainActivity extends AppCompatActivity implements TabLayout.OnTabSe
         //toolbar.setTitleTextColor(Color.BLACK);
         getSupportActionBar().setDisplayShowTitleEnabled(false);
         //getSupportActionBar (). setDisplayShowHomeEnabled (true);
-        getSupportActionBar().setIcon(R.drawable.icon);
+        getSupportActionBar().setIcon(R.drawable.icon4);
 
         HTfragment.push(new HomeTrainingFragment());
         TMfragment.push(new TrainerMatchFragment());
