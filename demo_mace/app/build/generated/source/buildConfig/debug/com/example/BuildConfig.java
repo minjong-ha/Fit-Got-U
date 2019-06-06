ca9bc392678421356ff5cfe7aa9a923051feff4f
hwangshkr <ruinahri@gmail.com>
2019-06-06 18:52:56 +0900

    merge test

--- demo_mace/app/build/generated/source/buildConfig/debug/com/example/BuildConfig.java
+++ demo_mace/app/build/generated/source/buildConfig/debug/com/example/BuildConfig.java
@@ -5,7 +5,7 @@ package com.example;
 
 public final class BuildConfig {
   public static final boolean DEBUG = Boolean.parseBoolean("true");
-  public static final String APPLICATION_ID = "android.example";
+  public static final String APPLICATION_ID = "com.example";
   public static final String BUILD_TYPE = "debug";
   public static final String FLAVOR = "";
   public static final int VERSION_CODE = 1;
