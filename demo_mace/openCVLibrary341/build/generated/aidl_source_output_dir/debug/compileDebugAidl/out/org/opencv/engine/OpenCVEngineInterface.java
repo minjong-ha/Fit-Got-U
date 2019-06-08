b1f324763e6abc8c2cbc0b8b6a74f1b8b7b36a75
JUH <jeonguh26@gmail.com>
2019-06-08 17:03:27 +0900

    first

--- demo_mace/openCVLibrary341/build/generated/aidl_source_output_dir/debug/compileDebugAidl/out/org/opencv/engine/OpenCVEngineInterface.java
+++ demo_mace/openCVLibrary341/build/generated/aidl_source_output_dir/debug/compileDebugAidl/out/org/opencv/engine/OpenCVEngineInterface.java
@@ -1,6 +1,6 @@
 /*
  * This file is auto-generated.  DO NOT MODIFY.
- * Original file: C:\\Users\\JiWookHwang\\Documents\\GitHub\\Fit-Got-U\\demo_mace\\openCVLibrary341\\java\\src\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
+ * Original file: C:\\Users\\JA\\Desktop\\Programming\\Git_folder\\Jelly\\final\\demo_mace\\openCVLibrary341\\java\\src\\org\\opencv\\engine\\OpenCVEngineInterface.aidl
  */
 package org.opencv.engine;
 /**
